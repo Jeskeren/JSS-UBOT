0bd8b11bb247a6a42901d80a6a9983d7fa5897e0
KY_UBOT <99032319+rizkypratama2@users.noreply.github.com>
2022-02-10 05:19:03 +0900

    Update ramfaction.py

--- userbot/modules/ramfaction.py
+++ userbot/modules/ramfaction.py
@@ -1,13 +1,14 @@
 # Port By @VckyouuBitch From Geez-Projects
+# recode from rizkypratama2
 # # Copyright (C) 2021 Geez-Project
 from userbot.events import register
 from userbot import CMD_HELP
 import asyncio
 
 
-@register(outgoing=True, pattern="^.ftyping(?: |$)(.*)")
-async def _(landak):
-    t = landak.pattern_match.group(1)
+@register(outgoing=True, pattern="^.ftype(?: |$)(.*)")
+async def _(rizky):
+    t = rizky.pattern_match.group(1)
     if not (t or t.isdigit()):
         t = 100
     else:
@@ -15,18 +16,18 @@ async def _(landak):
             t = int(t)
         except BaseException:
             try:
-                t = await landak.ban_time(t)
+                t = await rizky.ban_time(t)
             except BaseException:
-                return await landak.edit("`Incorrect Format`")
-    await landak.edit(f"`Kebanyakan fake hidup lu ngentot!`")
-    await landak.edit(f"`Memulai Fake Typing {t} detik.`")
-    async with landak.client.action(landak.chat_id, "typing"):
+                return await rizky.edit("`Incorrect Format`")
+    await rizky.edit(f"`Kebanyakan fake hidup lu ngentot!`")
+    await rizky.edit(f"`Memulai Fake Typing {t} detik.`")
+    async with rizky.client.action(rizky.chat_id, "typing"):
         await asyncio.sleep(t)
 
 
 @register(outgoing=True, pattern="^.faudio(?: |$)(.*)")
-async def _(landak):
-    t = landak.pattern_match.group(1)
+async def _(rizky):
+    t = rizky.pattern_match.group(1)
     if not (t or t.isdigit()):
         t = 100
     else:
@@ -34,17 +35,17 @@ async def _(landak):
             t = int(t)
         except BaseException:
             try:
-                t = await landak.ban_time(t)
+                t = await rizky.ban_time(t)
             except BaseException:
-                return await landak.edit("`Incorrect Format`")
-    await landak.edit(f"`Memulai Fake Audio dalam {t} detik.`")
-    async with landak.client.action(landak.chat_id, "record-audio"):
+                return await rizky.edit("`Incorrect Format`")
+    await rizky.edit(f"`Memulai Fake Audio dalam {t} detik.`")
+    async with rizky.client.action(rizky.chat_id, "record-audio"):
         await asyncio.sleep(t)
 
 
 @register(outgoing=True, pattern="^.fvideo(?: |$)(.*)")
-async def _(landak):
-    t = landak.pattern_match.group(1)
+async def _(rizky):
+    t = rizky.pattern_match.group(1)
     if not (t or t.isdigit()):
         t = 100
     else:
@@ -52,17 +53,17 @@ async def _(landak):
             t = int(t)
         except BaseException:
             try:
-                t = await landak.ban_time(t)
+                t = await rizky.ban_time(t)
             except BaseException:
-                return await landak.edit("`Incorrect Format`")
-    await landak.edit(f"`Memulai Fake video dalam {t} detik.`")
-    async with landak.client.action(landak.chat_id, "record-video"):
+                return await rizky.edit("`Incorrect Format`")
+    await rizky.edit(f"`Memulai Fake video dalam {t} detik.`")
+    async with rizky.client.action(rizky.chat_id, "record-video"):
         await asyncio.sleep(t)
 
 
 @register(outgoing=True, pattern="^.fgame(?: |$)(.*)")
-async def _(landak):
-    t = landak.pattern_match.group(1)
+async def _(rizky):
+    t = rizky.pattern_match.group(1)
     if not (t or t.isdigit()):
         t = 100
     else:
@@ -70,11 +71,11 @@ async def _(landak):
             t = int(t)
         except BaseException:
             try:
-                t = await landak.ban_time(t)
+                t = await rizky.ban_time(t)
             except BaseException:
-                return await landak.edit("`Incorrect Format`")
-    await landak.edit(f"`Memulai Fake Play game dalam {t} detik.`")
-    async with landak.client.action(landak.chat_id, "game"):
+                return await rizky.edit("`Incorrect Format`")
+    await rizky.edit(f"`Memulai Fake Play game dalam {t} detik.`")
+    async with rizky.client.action(rizky.chat_id, "game"):
         await asyncio.sleep(t)
 
 CMD_HELP.update(
