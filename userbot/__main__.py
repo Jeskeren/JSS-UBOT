b795401a69f86a008b0df33b4a6eddb8e6c846c5
Landak <80225366+ramadhani892@users.noreply.github.com>
2022-02-21 21:19:03 +0700

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -8,32 +8,32 @@
 import sys
 from importlib import import_module
 
-from telethon.tl.functions.channels import InviteToChannelRequest
-from userbot import ALIVE_NAME, BOT_USERNAME, BOT_VER, BOTLOG_CHATID, LOGS, UPSTREAM_REPO_BRANCH, bot
+from userbot import ALIVE_NAME, BOT_VER, BOTLOG_CHATID, LOGS, UPSTREAM_REPO_BRANCH, bot
 from userbot.modules import ALL_MODULES
 from userbot.utils.tools import hadeh_ajg
+
 try:
-    LOGS.info(f"⚡RAM - UBOT⚡ ⚙️ V{BOT_VER} [ TELAH DIAKTIFKAN! ]")
+    for module_name in ALL_MODULES:
+        imported_module = import_module("userbot.modules." + module_name)
+    bot.start()
+    LOGS.info(f"⚡Skyzu-Userbot⚡ ⚙️ V{BOT_VER} [ TELAH DIAKTIFKAN! ]")
 except BaseException as e:
     LOGS.info(str(e), exc_info=True)
     sys.exit(1)
 
 
-async def ram_ubot_on():
+async def kyy_ubot_on():
     try:
         if BOTLOG_CHATID != 0:
             await bot.send_message(
                 BOTLOG_CHATID,
-                f"💢 RAM - UBOT Berhasil Diaktfikan 💢\n╼┅━━━━━╍━━━━━┅╾\n❍▹ Bot Of : {ALIVE_NAME}\n❍▹ BotVer : {BOT_VER}@{UPSTREAM_REPO_BRANCH}\n╼┅━━━━━╍━━━━━┅╾",
+                f"⚡Userbot berhasil di aktifkan\n━━━━━━━━━━━━━━━\n❃ Bot Of : {ALIVE_NAME}\n❃ BotVer : {BOT_VER}@{UPSTREAM_REPO_BRANCH}\n━━━━━━━━━━━━━━━",
             )
     except Exception as e:
         LOGS.info(str(e))
-    try:
-        await bot(InviteToChannelRequest(int(BOTLOG_CHATID), [BOT_USERNAME]))
-    except BaseException:
-        pass
 
-bot.loop.run_until_complete(ram_ubot_on())
+
+bot.loop.run_until_complete(kyy_ubot_on())
 bot.loop.run_until_complete(hadeh_ajg())
 if len(sys.argv) not in (1, 3, 4):
     bot.disconnect()
