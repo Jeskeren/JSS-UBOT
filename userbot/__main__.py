5dc88517fb6f71324d4fc9175949a1fe8a761c78
Landak <80225366+ramadhani892@users.noreply.github.com>
2022-02-21 22:07:58 +0700

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -16,7 +16,7 @@ try:
     for module_name in ALL_MODULES:
         imported_module = import_module("userbot.modules." + module_name)
     bot.start()
-    LOGS.info(f"⚡Skyzu-Userbot⚡ ⚙️ V{BOT_VER} [ TELAH DIAKTIFKAN! ]")
+    LOGS.info(f"🤙🏻𝗞𝘆𝘆-𝗨𝗯𝗼𝘁🤙🏻 ⚙️ V{BOT_VER} [ TELAH DIAKTIFKAN! ]")
 except BaseException as e:
     LOGS.info(str(e), exc_info=True)
     sys.exit(1)
