2bd0f68e686d5aa2ec02842df2ce188f94e576dc
MERDHANI <80225366+ramadhani892@users.noreply.github.com>
2022-03-31 18:01:29 +0700

    Update ramcarbon.py

--- userbot/modules/ramcarbon.py
+++ userbot/modules/ramcarbon.py
@@ -163,7 +163,7 @@ all_col = [
 ]
 
 
-@register(outgoing=True, pattern=r"^\.(?:rcarbon|carbon)\s?(.)?")
+@register(outgoing=True, pattern=".(rc|c)arbon")
 async def crbn(event):
     from_user = vcmention(event.sender)
     xxxx = await edit_or_reply(event, "`Processing...`")
